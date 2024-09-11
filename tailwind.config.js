bb9c72c8e1ef32ae489c21a31248c9e2c92a3469
Ayesha Rashid Khan <aarkk.29@gmail.com>
2024-09-12 01:16:32 +0500

    configured font style clash-display in the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        clash: ["var(--font-clash-display)"],
+      },
+    },
   },
   plugins: [],
 };
