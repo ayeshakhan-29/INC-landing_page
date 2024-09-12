b6b090baccdf2b671cf939943905c8aefa37dfb9
Ayesha Rashid Khan <aarkk.29@gmail.com>
2024-09-13 01:21:31 +0500

    created join waitlist page

---
+++ app/join-waitlist/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import JoinWaitlist from "../components/modules/join-waitlist/join-waitlist";
+
+const JoinWaitlistPage = () => {
+  return (
+    <div>
+      <JoinWaitlist />
+    </div>
+  );
+};
+
+export default JoinWaitlistPage;
