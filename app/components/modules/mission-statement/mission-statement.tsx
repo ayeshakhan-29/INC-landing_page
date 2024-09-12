6ab3547b33731b789e0baa9294ed9e184b406468
Ayesha Rashid Khan <aarkk.29@gmail.com>
2024-09-13 02:33:02 +0500

    created mission statement page (responsive)

---
+++ app/components/modules/mission-statement/mission-statement.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const MissionStatement = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-full lg:min-h-screen font-clash my-10 md:my-20 lg:my-0 mx-4 md:mx-20 lg:mx-40 text-[#101623] gap-4">
+      <div className="flex flex-col items-center justify-center text-center lg:gap-0 gap-3">
+        <p className="text-base">
+          The Mission of the Incc is to celebrate culture, community and
+          inclusion.
+        </p>
+        <p className="text-4xl md:text-5xl lg:text-[70px]">Mission Statement</p>
+        <div className="flex h-[1px] w-60 md:w-80 bg-black"></div>
+      </div>
+
+      <p className="text-center text-lg leading-7">
+        The Mission of the Incc is to celebrate culture & community with
+        original storytelling and social networking.
+        <br />
+        <br /> The Incc is a Multicultural Media & Social Networking Platform
+        created to help solve the problem of how to bring cultures closer
+        together in celebration of not only themselves, but of one another. The
+        Incc creates original profiles and stories on cultures and communities
+        about not only who we are, but also how we are all bound to one another
+        in this shared humanity.
+        <br /> <br /> We are driven by digital magazines that are dedicated to
+        culture and a community side dedicated to all the best of social
+        networking and more. We impact go-getters, visionaries, culture-lovers,
+        content creators, corporate leaders, entrepreneurs, dreamers, activists
+        & advocates, students, mediators, brand builders, moms, dads, the
+        educated and educators.
+        <br /> <br /> Most importantly, our mission is to build cultural bridges
+        around our content and social platform so we can learn more about one
+        another, connect, network, build and grow.
+        <br /> <br /> Creating stories about not only who we are, but also how
+        we are bound to one another.
+      </p>
+      <p className="text-2xl font-normal">Welcome to The Inccc</p>
+    </div>
+  );
+};
+
+export default MissionStatement;
---
+++ app/mission-statement/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MissionStatement from "../components/modules/mission-statement/mission-statement";
+
+const MissonStatementPage = () => {
+  return <MissionStatement />;
+};
+
+export default MissonStatementPage;
