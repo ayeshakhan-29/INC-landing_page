67f8ae444631f43c030df54e1ec7b060ed9f432c
Ayesha Rashid Khan <aarkk.29@gmail.com>
2024-09-17 01:30:42 +0500

    added content in terms & confditions page and mapped data

---
+++ app/components/modules/terms-and-conditions/t-and-c.tsx
@@ -0,0 +1,136 @@
+import React from "react";
+const termsData = [
+  {
+    title: "1. OUR TERMS",
+    content: `Welcome to the website of TheIncc.<br><br>
+    These "Terms of Service" ("Agreement") regulate and control your access and general use. The inclusion of terms such as "We," "Our," "Us," and "Company" in these "Terms of Service" document signify the representation of TheIncc whereas terms like "You," "Your," or "Client" belong to represent the user or client of TheIncc.<br><br>
+    The below "Terms of Service" ("Terms") are a legally binding contract between you and TheIncc. They provide clear guidelines for your use of our services and website. By accessing or using our Services and Website, you acknowledge that you have read and agree to these Terms of Service and confirm your understanding of our Privacy Policy.<br><br>
+    If you do not agree to these Terms, please do not access or use the Service or Website.`,
+  },
+  {
+    title: "2. SERVICES WE OFFER",
+    content: `Our platform act as a multicultural media and social networking platform that provides its users with various news channels available for subscription. This platform allows the users to share content, including videos, blogs, and pictures, and earn revenue by allowing their followers to subscribe. These terms and conditions govern the use of Service or Website and outline the rights and responsibilities of its users.`,
+  },
+  {
+    title: "3. USER ACCOUNT",
+    content: `To use our Services and Website, you must create a user account. When creating your account, you must provide accurate and complete information. You are responsible for maintaining the confidentiality of your account and responsible for restricting access to your account. You agree to accept responsibility for all activities that occur under your account.`,
+  },
+  {
+    title: "4. USER REPRESENTATIONS",
+    content: `Our platform allows users to share their content for free. However, you are solely responsible for the content that you make available through the platform, including any consequences of its use. When using the Site, you confirm and warrant that: <br/>(a) you have the necessary legal capacity and will abide by these Terms of Use; <br/> (b) you must be of 16 years of age or any legal age according to the laws of your jurisdiction; <br/> 
+    (c) you will not access the Site using automated or non-human methods, such as bots, scripts, or any other means; <br/> 
+    (d) you will not engage in any illegal or unauthorized activities on the Site; and <br/> 
+    (e) your use of the Site will not violate any relevant laws or regulations; <br/> 
+    (f) You agree to act responsibly and legally when using our services. Your usage should not be detrimental, offensive, or inconvenient to any other internet user; <br/> 
+    (g) You either own the content or have the right to use or share it; <br/> 
+    (h) You must adhere that the content does not infringe any third-party rights; <br/> 
+    (g) You agree not to use the Platform to: i. Create a false identity on the Platform, impersonate any person or entity or falsely state or otherwise misrepresent yourself, your name, your age or your affiliation with any person or entity. <br/> ii. Use the Platform for any illegal purpose, criminal or tortious activity, including but not limited to, fraud, spamming, sending of viruses or other harmful files, copyright infringement <br/> iii. Upload, post, email, transmit or otherwise make available any content or initiate communications that include information that you do not have the right to disclose or make available under any law or contractual or other relationships (such as proprietary and confidential information learned or disclosed as part of employment relationships or under nondisclosure agreements) or which infringes any intellectual property rights; <br/> iv. Upload, post, email, transmit or otherwise make available any unsolicited or unauthorized advertising, promotional materials, junk mail, spam, chain letters, pyramid schemes or any other form of solicitation; <br/> v. Upload, post, email, transmit or otherwise make available any material that contains viruses, Trojan horses, worms, time-bombs, keystroke loggers, spyware, adware or any other harmful programmes or similar computer code, files or programs designed to interrupt, destroy, limit or adversely affect the functionality of any computer software or hardware or telecommunications equipment; <br/> vi. Attempt to gain unauthorized access to the Platform, in particular the non-public areas, the server on which the Platform is stored or any server, computer or database connected to the Platform: <br/> vii. Attack the Platform via a denial-of-service attack or a distributed denial-of-service attack; <br/> viii. Interfere with or disrupt our services or servers or networks connected to this Platform, or disobey any requirements, procedures, policies or regulations of networks connected to this Platform: <br/> ix. Use in connection with the Platform any pirated software or any copyrighted materials or counterfeit materials without the permission of the copyright holder; or <br/> x. Use in connection with the Platform any materials which in any way infringe the copyright, other intellectual property rights or proprietary rights of a third party or which may otherwise render us liable to the payment of damages to any third party. <br/> (h) We reserve the right to investigate and take appropriate legal action against anyone who, in our opinion, violates this provision, including, without limitation, reporting you to appropriate law enforcement authorities. <br/> (i) We reserve the right to remove or suspend any material posted which is false, incorrect, outdated, incomplete information or in our sole opinion, in breach of this clause, or which we suspect to be in breach of this clause at our absolute discretion. We shall be entitled to terminate your registration for breach of this clause and retains the right to deny any present or future utilization of our Site (or any part thereof). <br/>`,
+  },
+  {
+    title: "5. CODE OF CONDUCT",
+    content: ` i. Any form of hate will not be tolerated in any content.<br/> 
+    ii. The posting of any kind of pornographic content is strictly prohibited.<br/> 
+    iii. Harassment, cyberbullying, and stalking are not allowed and will result in removal.<br/> 
+    iv. Engage in factual and opinion-based discussions; disagreements are acceptable, but avoid using slurs and refrain from name-calling.<br/> 
+    v. Financial scams or predatory actions of any nature will result in removal.<br/> 
+    vi. Attempts to fraudulently use someone's data or identity will result in removal.<br/> 
+    vii. Adhere to the principle that if an action is illegal in the real world, it is also illegal on this platform.<br/> 
+    viii. The platform reserves the right to remove users if their behaviour is considered detrimental by the admin or the community.<br/>`,
+  },
+  {
+    title: "6. ADULT CONTENT OR SEXUAL CONTENT",
+    content: `We limit the presentation of nudity or sexual activity in any form, including images and videos, as some members of our community may find such content sensitive. Moreover, we automatically remove sexual imagery to prevent the dissemination of non-consensual or underage content. The guidelines concerning the depiction of sexual activity extend to digitally generated content, unless it is shared for educational, humorous, or satirical purposes.`,
+  },
+  {
+    title: "7. CONTENT SHARING AND SUBSCRIPTION",
+    content: `Users can share content on the Platform, including videos, blogs, and pictures. Users also have the option to subscribe to news channels and earn revenue by allowing their followers to subscribe to their content. Users agree not to share content that violates the law or infringes on the rights of others. You understand that this website is for a profit company and will retain a portion of the subscription fees as compensation for its services.`,
+  },
+  {
+    title: "8. INTELLECTUAL PROPERTY",
+    content: `Users maintain ownership of their content, and by sharing it on the Platform, they provide The Incc with a global, non-exclusive, royalty-free license for using, reproducing, modifying, and distributing the content. The Incc’s content, encompassing software, graphics, text, images, and other materials, is safeguarded by intellectual property laws, including copyright and trademark laws. Use of our website or its content for commercial purposes requires explicit written consent from The Incc Incorporated.`,
+  },
+  {
+    title: "9. REVENUE EARNING AND PAYMENTS",
+    content: `Users may earn revenue through subscriptions to their content. We will facilitate payments to users based on the terms outlined in the revenue sharing agreement.`,
+  },
+  {
+    title: "10. PRIVACY",
+    content: `Your use of the Website and Services is subject to our Privacy Policy which outlines how we collect, use, and protect your personal information. By using the Website, you consent to the collection, use and sharing (as described in the Privacy Policy) of your data or information, including the transfer of this information to the United States and/or other countries for storage, processing, and use by The Incc.`,
+  },
+  {
+    title: "11. PROHIBITED ACTIVITIES",
+    content: `The Incc requires everyone to be at least 16 years old before they can create an account. Users agree not to engage in activities that violate these Terms, including but not limited to, unauthorized access, interference with the Platform, and the creation of multiple accounts.`,
+  },
+  {
+    title: "12. COMPLIANCE WITH COMMUNITY GUIDELINES",
+    content: `You agree that you will comply with Community Guidelines.`,
+  },
+  {
+    title: "13. TERMINATION AND DELETION",
+    content: `Upon the termination of your account or the deletion of specific User Content from The Incc, we will exert reasonable efforts to render such User Content inaccessible, cease its use, and, if stipulated by the relevant Privacy Policy, erase your Account data and/or User Content, unless legal obligations permit or necessitate data retention. Nonetheless, it is acknowledged and agreed that: <br/>
+    (a) deleted User Content may persist in caches or backups for a reasonable duration, and <br/>
+    (b) complete removal of copies or references to the User Content may not be achievable, considering factors such as resharing. The users also have the option to delete your account at any time.`,
+  },
+  {
+    title: "14. LIMITATION OF LIABILITY",
+    content: `TO THE FULLEST EXTENT PERMITTED BY APPLICABLE LAW, THE INCC/THE INCUBATOR WILL NOT BE HELD LIABLE FOR: (A) ANY INDIRECT, INCIDENTAL, EXEMPLARY, PUNITIVE, OR CONSEQUENTIAL DAMAGES OF ANY NATURE;(B) THE LOSS OF PROFITS, REVENUE, DATA, USE, GOODWILL, OR OTHER INTANGIBLE LOSSES;(C) DAMAGES RELATED TO YOUR ACCESS TO, USE OF, OR INABILITY TO ACCESS OR USE THE SERVICES;(D) DAMAGES RELATED TO ANY CONDUCT OR CONTENT OF ANY THIRD PARTY OR USER USING THE SERVICES, INCLUDING, BUT NOT LIMITED TO, DEFAMATORY, OFFENSIVE, OR ILLEGAL CONDUCT OR CONTENT;AND/OR (E) DAMAGES IN ANY MANNER RELATED TO ANY CONTENT. THIS LIMITATION ENCOMPASSES ALL CLAIMS, REGARDLESS OF WHETHER THEY ARE BASED ON WARRANTY, CONTRACT, TORT, OR ANY OTHER LEGAL THEORY, AND APPLIES EVEN IF WE WERE NOT INFORMED OF THE POSSIBILITY OF SUCH DAMAGE. <br/><br/>`,
+  },
+  {
+    title: "15. CHANGES TO TERMS",
+    content: `We reserve the right to amend these Terms of Service or our Community Guidelines and will inform you of such changes. Notice will be given by posting a revised version of the modified Terms of Service or Community Guidelines on the Services. In some instances, additional notice may be provided, such as through email or within the Services (e.g., via a notification on your Dashboard or in our mobile applications). It is important to note that modifications will not have retrospective application. Your responsibility includes reviewing and acquainting yourself with any alterations made to this Agreement.`,
+  },
+  {
+    title: "16. GOVERNING LAW AND JURISDICTION",
+    content: `
+You and The Incc agree that we will resolve any claim or controversy at law or equity that arises out of this Agreement or the Services in accordance with this Section or as you and we otherwise agree in writing. Before resorting to formal dispute resolution, we strongly encourage you to contact us to seek a resolution.
+ <br/><br/>
+This Agreement shall be governed in all respects by the laws of the State of …………as they apply to agreements entered into and to be performed entirely within ……….. between …………….. residents, without regard to conflict of law provisions. You agree that any claim or dispute you may have against The Incc must be resolved exclusively by a state or federal court located in ……. County, …………, except as otherwise agreed by the parties. You agree to submit to the personal jurisdiction of the courts located within ……. County, ……… for the purpose of litigating all such claims or disputes.<br/><br/>
+If you are (a) a United States federal, state, or local government agency or body, (b) using the Services in your official capacity, and <br/>(c) legally unable to accept the clauses in this Section, then this Section doesn’t apply to you. For such entities, this Agreement and any related action will be governed by the laws of the United States of America, without regard to conflict of law provisions, and, in the absence of federal law and to the extent permitted under federal law, the laws of the State of ………..
+
+`,
+  },
+  {
+    title: "17. CHANGE OF OWNERSHIP",
+    content: `In the event of a change of ownership or control of all or a part of Our Website, including without limitation through acquisition, merger, or sale, we reserve the right to transfer all or part of the User Data and Public Data we store in our systems. You acknowledge that in the event of bankruptcy, insolvency, or receivership, we may have no control over the use and transfer of your personal information.`,
+  },
+  {
+    title: "18. ENTIRE AGREEMENT",
+    content: `The failure of us to exercise or enforce any right or provision of these “Terms and Conditions of Services” shall not constitute a waiver of such right or provision. These “Terms and Conditions of Services” and any policies or operating rules posted by us on this site or concerning the service constitute the entire agreement and understanding between you and us and govern your use of the Service, superseding any prior or contemporaneous agreements, communications and proposals, whether oral or written, between you and us (including, but not limited to, any prior versions of the “Terms and Conditions of Services”).`,
+  },
+  {
+    title: "19. CONTACT INFORMATION",
+    content: `For any questions or concerns regarding these Terms, please contact us at youremail@gmail.com<br/>
+    emailemail.<br>
+   By using this Platform, you agree to abide by these Terms of Service. Thank you for being a part of The Incc`,
+  },
+];
+
+const TermsAndConditions = () => {
+  return (
+    <div className="min-h-screen bg-white py-16 mx-4 lg:mx-28 xl:mx-32 2xl:mx-40 font-clash">
+      <div className="text-left">
+        <h1 className="text-4xl md:text-5xl lg:text-[70px] text-[#101623] uppercase">
+          Terms & Conditions of Services
+        </h1>
+
+        <div className="flex h-[1px] w-60 md:w-80 bg-black mt-4"></div>
+
+        <div className="mt-12 text-[#101623] space-y-10">
+          {termsData.map((section, index) => (
+            <div key={index}>
+              <h2 className="lg:text-[30px] md:text-2xl text-xl font-normal uppercase">
+                {section.title}
+              </h2>
+              <p
+                className="mt-2 lg:mt-8 leading-7 text-lg"
+                dangerouslySetInnerHTML={{ __html: section.content }}
+              ></p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TermsAndConditions;
