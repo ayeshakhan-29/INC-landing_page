b64e59cd714bd49a09dc4b204edd42ee5de418e5
Ayesha Rashid Khan <aarkk.29@gmail.com>
2024-09-17 01:30:26 +0500

    created terms & confditions page

---
+++ app/terms-and-conditions/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TermsAndConditions from "../components/modules/terms-and-conditions/t-and-c";
+
+const termsandconditionsPage = () => {
+  return <TermsAndConditions />;
+};
+
+export default termsandconditionsPage;
