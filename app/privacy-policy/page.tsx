7ff3584a752d6efdb803f970472e614ecedd1124
Ayesha Rashid Khan <aarkk.29@gmail.com>
2024-09-16 22:55:18 +0500

    created privacy policy page

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import PrivacyPolicy from "../components/modules/privacy-policy/privacy-policy";
+const PrivacyPolicyPage = () => {
+  return <PrivacyPolicy />;
+};
+
+export default PrivacyPolicyPage;
